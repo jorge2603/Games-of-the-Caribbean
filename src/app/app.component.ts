90a8bbf4a795ba5be39bc5a5f81318302ba21475
Heigz <UchihaItachi@ciencias.unam.mx>
2024-03-05 17:14:26 -0600

    hotfix:Se modifico la manera de implementar el script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
   title = 'Games-of-the-Caribbean';
 
-  
+
+  toggleTheme() {
+    const element = document.body;
+    element.dataset['bsTheme'] = element.dataset['bsTheme'] === 'dark' ? 'light' : 'dark';
+  }
 }
+
+
+
